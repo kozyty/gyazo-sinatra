d4461fcbca2e0dfddb8ff91425135ba4d1bae0e0
kozyty <me@kozyty.com>
2012-12-11 16:40:51 +0900

    Init gyazo.kozyty.com settings.

--- app.rb
+++ app.rb
@@ -9,19 +9,23 @@ module Gyazo
 
     configure do
       set :dbm_path, 'db/id'
-      set :image_dir, 'public/images'
-      set :image_url, 'http://gyazo.send.sh/images'
+      set :image_dir, "public/images/"
+      set :image_url, "http://gyazo.kozyty.com/images/"
     end
 
     post '/' do
-      id = request[:id]
+      id   = request[:id]
       data = request[:imagedata][:tempfile].read
       hash = Digest::MD5.hexdigest(data).to_s
-      dbm = SDBM.open(options.dbm_path, 0644)
+      dbm  = SDBM.open(options.dbm_path, 0644)
       dbm[hash] = id
-      File.open("#{options.image_dir}/#{hash}.png", 'w'){|f| f.write(data)}
+      image_dir = "#{options.image_dir}#{Time.now.strftime("%Y/%m/%d")}"
+      image_url = "#{options.image_url}#{Time.now.strftime("%Y/%m/%d")}"
+      FileUtils.mkdir_p(image_dir) unless FileTest.exist?(image_dir)
+      File.open("#{image_dir}/#{hash}.png", 'w'){|f| f.write(data)}
 
-      "#{options.image_url}/#{hash}.png"
+      "#{image_url}/#{hash}.png"
     end
+
   end
 end
