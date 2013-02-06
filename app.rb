f668ad9269e98ecc8fdfebbbce0bcae3ca3342ae
kozyty <me@kozyty.com>
2013-02-06 15:44:49 +0900

    add index/image on twitterbootstrap

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require 'sinatra'
 require 'rack'
 require 'digest/md5'
 require 'sdbm'
+require 'haml'
 
 module Gyazo
   class Controller < Sinatra::Base
@@ -10,6 +11,7 @@ module Gyazo
     configure do
       set :dbm_path, 'db/id'
       set :image_dir, "public/images/"
+      set :send_url, "http://gyazo.kozyty.com/"
       set :image_url, "http://gyazo.kozyty.com/images/"
     end
 
@@ -25,13 +27,16 @@ module Gyazo
       dbm[hash] = id
       image_dir = "#{options.image_dir}#{Time.now.strftime("%Y/%m/%d")}"
       image_url = "#{options.image_url}#{Time.now.strftime("%Y/%m/%d")}"
+      send_url = "#{options.send_url}#{Time.now.strftime("%Y/%m/%d")}"
       FileUtils.mkdir_p(image_dir) unless FileTest.exist?(image_dir)
       File.open("#{image_dir}/#{hash}.png", 'w'){|f| f.write(data)}
 
-      "#{image_url}/#{hash}.png"
+      "#{send_url}/#{hash}"
     end
 
-    get '/*' do
+    get %r{/(\d+\/\d+\/\d+\/\w+)$} do |capture|
+      @url = "#{options.send_url}#{capture}"
+      @image_url = "#{options.image_url}#{capture}.png"
       haml :image
     end
 
